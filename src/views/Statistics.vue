85980cb84a07111ca6f5f4c6139b9eeffc56bac0
crush <crush_mar@163.com>
2021-07-24 15:47:29 +0800

    封装布局

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      Statistics.vue
-    </div>
-    <Nav/>
-  </div>
+      <layout>
+        Statistics.vue
+      </layout>
+
 </template>
 
 <script lang="ts">
@@ -14,14 +12,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper {
-  border: 1px solid green;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-.content{
-  overflow: auto;
-  flex-grow: 1;
-}
 </style>
